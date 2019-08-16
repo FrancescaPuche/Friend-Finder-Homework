442580ccfd510942be8fb6ef6e8b6d0e2a4c4ccc
Francesca P <francescapuche@gmail.com>
2019-08-16 01:45:16 -0400

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express"); 
+const path = require("path"); 
+
+const app = express(); 
+const PORT = process.env.PORT || 8080; 
+
+app.use(express.static("./app/public")); 
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
+
+app.use(express.static(__dirname + "./app/public/css")); 
+
+require("./app/routes/apiRoutes")(app); 
+require("./app/routes/htmlRoutes")(app); 
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
