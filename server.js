30d9cb4e7ea5fe728808bed1f22b1a940ac8bd66
Francesca P <francescapuche@gmail.com>
2019-08-16 23:53:47 -0400

    testing different port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path"); 
 
 const app = express(); 
-const PORT = process.env.PORT || 8080; 
+const PORT = process.env.PORT || 3003; 
 
 app.use(express.static("./app/public")); 
 app.use(express.urlencoded({extended: true})); 
