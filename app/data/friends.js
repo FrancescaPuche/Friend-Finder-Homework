b03e42a0f0b63b39aca3871ea851b59cd8256d4d
Francesca P <francescapuche@gmail.com>
2019-08-16 23:18:18 -0400

    added friends

---
+++ app/data/friends.js
@@ -0,0 +1,131 @@
+const friends = [{
+    "name": "Jack Durry",
+    "photo": "https://randomuser.me/api/portraits/men/81.jpg",
+    "scores": [
+        2,
+        3,
+        5,
+        4,
+        1,
+        2,
+        2,
+        4,
+        1,
+        5
+    ]
+},
+{
+    "name": "Jane Doe",
+    "photo": "https://randomuser.me/api/portraits/women/21.jpg",
+    "scores": [
+        4,
+        5,
+        3,
+        4,
+        5,
+        4,
+        1,
+        1,
+        2,
+        2
+    ]
+},
+{
+    "name": "Jenny Doie",
+    "photo": "https://randomuser.me/api/portraits/men/17.jpg",
+    "scores": [
+        1,
+        3,
+        3,
+        5,
+        2,
+        1,
+        3,
+        4,
+        4,
+        3
+    ]
+},
+{
+    "name": "Jannis Dooe",
+    "photo": "https://randomuser.me/api/portraits/women/8.jpg",
+    "scores": [
+        3,
+        5,
+        2,
+        4,
+        4,
+        1,
+        5,
+        3,
+        5,
+        4
+    ]
+},
+{
+    "name": "Johnny Du",
+    "photo": "https://randomuser.me/api/portraits/men/54.jpg",
+    "scores": [
+        1,
+        3,
+        1,
+        3,
+        5,
+        5,
+        4,
+        3,
+        4,
+        2
+    ]
+},
+{
+    "name": "Jake Dae",
+    "photo": "https://randomuser.me/api/portraits/men/38.jpg",
+    "scores": [
+        5,
+        1,
+        3,
+        1,
+        2,
+        1,
+        2,
+        4,
+        5,
+        1
+    ]
+},
+{
+    "name": "Jean Dee",
+    "photo": "https://randomuser.me/api/portraits/men/74.jpg",
+    "scores": [
+        5,
+        1,
+        1,
+        2,
+        3,
+        4,
+        1,
+        5,
+        3,
+        1
+    ]
+},
+{
+    "name": "June Dope",
+    "photo": "https://randomuser.me/api/portraits/women/32.jpg",
+    "scores": [
+        3,
+        1,
+        1,
+        5,
+        5,
+        1,
+        3,
+        5,
+        4,
+        5
+    ]
+}
+];
+
+module.exports = friends;
