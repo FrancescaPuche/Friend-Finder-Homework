2b867bc3c504a6936356816c5d883a652406347b
Francesca P <francescapuche@gmail.com>
2019-08-16 23:31:09 -0400

    loops to go through survey and compare scores

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -7,9 +7,27 @@ module.exports = (app) => {
     });
 
     app.post("/api/friends", (req, res) => {
-        let friendscore = req.body.scores;
+        let friendScore = req.body.scores;
         const scoresArr = [];
         let bestMatch = 0; 
-        
+
+        for (var i = 0; i < friends.length; i++) {
+            var scoreDiff = 0;
+            for (var j = 0; j < friendScore.length; j++) {
+                scoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(friendScore[j])))
+            }
+            scoresArr.push(scoreDiff);
+        }
+
+        for (var i = 0; i < scoresArr.length; i++) {
+            if (scoresArr[i] <= scoresArr[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        let bestFriend = friendScore[bestMatch];
+        res.json(bestFriend);
+        friendScore.push(req.body)
+
     });
 };
